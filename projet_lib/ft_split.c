66673b15287ffedd175a00492894c9f13301420f
balkisous <balkisous@gmail.com>
2021-01-07 17:12:46 +0100

    split finish du 07\01\2021

--- projet_lib/ft_split.c
+++ projet_lib/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 11:57:48 by bben-yaa          #+#    #+#             */
-/*   Updated: 2021/01/07 10:00:54 by bben-yaa         ###   ########.fr       */
+/*   Updated: 2021/01/07 17:09:23 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,18 @@ int ft_count(char *s, char sep)
 
 	i = 0;
 	count = 0;
-
+	if (s[0] != sep)
+	{
+		count++;
+	}
 	while(s[i])
 	{
-		if (s[i] == sep && s[i + 1] != sep && s[i + 1] != '\0')
+		if (s[i - 1] == sep && s[i] != sep)
+		{
 			count++;
+		}
 		i++;
 	}
-	printf("count vaut %d\n", count);
 	return (count);
 }
 
@@ -49,8 +53,37 @@ char *ft_strdup_sep(char *s, char sep)
 		i++;
 	}
 	dest[i] = '\0';
-	printf("%s\n", dest);
 	return(dest);
+}
+
+char** ft_fill(int j, char **new, char *s, char c)
+{
+	int k;
+	int l;
+
+	k = 0;
+	l = 0;
+	if (s[0] != c && k < j)
+	{
+		new[k] = ft_strdup_sep(&s[0], c);
+//		printf("here\n");
+		printf("%s\n", new[0]);
+		k++;
+//		printf("k dasn 0 %d\n", k);
+	}
+	while(s[l] && k < j)
+	{
+		if (s[l - 1] == c && s[l] != c) 
+		{
+
+//			printf("k dasn lol  %d\n", k);
+			new[k] = ft_strdup_sep(&s[l], c);
+			printf("%s\n", new[k]);
+			k++;
+		}
+		l++;
+	}
+	return(new);
 }
 
 char **ft_split(char const *s, char c)
@@ -59,43 +92,30 @@ char **ft_split(char const *s, char c)
 	char	*str;
 	int	i;
 	int 	j;
-	int 	k;
 
 	str = (char *)s;
 	i = 0;
-	k = 1;
 	j = ft_count(str, c);
 	if (str == 0)
 		return(0);
 	new = malloc(sizeof(char) * j);
-		if(new == NULL)
-			return (0);
-	j = 0;
-	while (str[i])
-	{
-		printf("j vaut %d\n", j);
-		if (str[j - 1] == c && str[j] != c)
-		{
-			printf("first i = %d\n", i);
-			new[k] = ft_strdup_sep(&str[i], c);
-			k++;
-			printf("i avant while vaut %d\n", i);
-			while (!(str[i] = c && str[i + 1] != c))
-				i++;
-			printf("i apres while vaut %d\n", i);
-		}
-		j++;
-	}
-	return(new);
+	if(new == NULL)
+		return (0);
+	new = ft_fill(j, new, str, c);
+	printf("new[0] %s\n", new[0]);
+	printf("new[1] %s\n", new[1]);
+	printf("new[2] %s\n", new[2]);
+	printf("new[3] %s\n", new[3]);
+	printf("new[4] %s\n", new[4]);
+	return (new);
+
 }
 #include <stdio.h>
 int main ()
 {
-	char *s = "Hello     les      gens";
+	char *s ="Hello   Coucou    les amis  berk";	
 	char c = ' ';
-//	int i = 10;
 	ft_split(s, c);
-//	printf("count == %d\n", ft_count(s, c));
-//	printf("strdup = %s\n", ft_strdup_sep(&s[i], c));
+//	printf("%d\n", ft_count(s, c));
 	return(0);
 }
