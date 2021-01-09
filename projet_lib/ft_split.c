1657bf4735b63ca756b20ea39b0b7a9f8fec7fa0
Balkis BEN YAAGOUB <bben-yaa@e3r4p2.42.fr>
2021-01-09 10:49:42 +0100

    split fini le 09/01/2021

--- projet_lib/ft_split.c
+++ projet_lib/ft_split.c
@@ -66,26 +66,17 @@ char** ft_fill(int j, char **new, char *s, char c)
 	if (s[0] != c && k < j)
 	{
 		new[0] = ft_strdup_sep(&s[0], c);
-		printf("%s\n", new[0]);
 		k++;
-//		printf("k dasn 0 %d\n", k);
 	}
 	while(s[l] && k < j)
 	{
 		if (s[l - 1] == c && s[l] != c) 
 		{
-
-//			printf("k dasn lol  %d\n", k);
 			new[k] = ft_strdup_sep(&s[l], c);
-			printf("%s\n", new[k]);
-
-			printf("dans la boucle qui rempli, la premiere ligne %s\n", new[0]);
-			printf("dans la boucle la premiere lettre %c\n", new[0][0]);
 			k++;
 		}
 		l++;
 	}
-	printf("la premiere ligne %s\n", new[0]);
 	return(new);
 }
 
@@ -101,28 +92,24 @@ char **ft_split(char const *s, char c)
 	j = ft_count(str, c);
 	if (str == 0)
 		return(0);
-	new = malloc(sizeof(char) * j + 1);
+	new = malloc(sizeof(char *) * j + 1);
 	if(new == NULL)
 		return (0);
 	new = ft_fill(j, new, str, c);
-
-	printf("j = %d\n", j);
-	new[j ] = NULL;
-	printf("new[0] %s\n", new[0]);
-	printf("new[1] %s\n", new[1]);
-	printf("new[2] %s\n", new[2]);
-	printf("new[3] %s\n", new[3]);
-	printf("new[4] %s\n", new[4]);
-	printf("new[5] %s\n", new[5]);
+	new[j] = NULL;
+	while(i <= j)
+	{
+		printf("%s\n", new[i]);
+		i++;
+	}
 	return (new);
 
 }
 #include <stdio.h>
 int main ()
 {
-	char *s ="Hello coucou  les amis berk";	
+	char *s ="   Hello coucou  les amis berk   comment vous   allez haha   ";	
 	char c = ' ';
 	ft_split(s, c);
-//	printf("%d\n", ft_count(s, c));
 	return(0);
 }
